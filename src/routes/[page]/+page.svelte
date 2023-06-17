b84677a68cbd6d359159e6e09691c874262802de
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-06-17 14:57:34 +0900

    Fix: レスポンシブ周りを修正

--- src/routes/[page]/+page.svelte
+++ src/routes/[page]/+page.svelte
@@ -93,8 +93,9 @@
 		}
 		:global(img) {
 			width: 100%;
+      box-sizing: border-box;
 			filter: drop-shadow(0 0 0.5rem #ddd);
-			margin: 1rem;
+			padding: 1rem 0;
 			display: block;
 
 			@media (min-width: 768px) {
